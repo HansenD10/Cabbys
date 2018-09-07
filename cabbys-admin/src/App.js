94d0fd18720941d8326b671e459923766b4c12af
HansenD10 <dylan.hansen10@gmail.com>
2018-09-07 00:45:48 -0500

    Adding api calls to get initial state.

--- cabbys-admin/src/App.js
+++ cabbys-admin/src/App.js
@@ -1,46 +1,48 @@
 import React, { Component } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
-// import axios from 'axios'
+import axios from 'axios'
 import Login from './Auth/Login'
 import Admin from './Components/Admin/Admin'
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   // this.state = {
-  //   //   data: {
-  //   //     foods: [],
-  //   //     drinks: [],
-  //   //     events: [],
-  //   //     contact: []
-  //   //   }
-  //   // }
-  // }
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: {
+        foods: [],
+        drinks: [],
+        events: [],
+        contact: []
+      }
+    }
+  }
 
-  // async componentWillMount() {
-  //   let foods = await axios.get("./api/foods"),
-  //     drinks = await axios.get("./api/drinks"),
-  //     events = await axios.get("./api/events"),
-  //     contact = await axios.get("./api/contact"),
-  //     hours = await axios.get("./api/hours")
+  async componentWillMount() {
+    let foods = await axios.get("../api/foods"),
+      drinks = await axios.get("../api/drinks"),
+      events = await axios.get("../api/events"),
+      contact = await axios.get("../api/contact"),
+      hours = await axios.get("../api/hours")
 
-  //   this.setState({
-  //     ...this.state,
-  //     data: {
-  //       foods: foods.data,
-  //       drinks: drinks.data,
-  //       events: events.data,
-  //       contact: contact.data,
-  //       hours: hours.data
-  //     }
-  //   })
-  // }
+    this.setState({
+      ...this.state,
+      data: {
+        foods: foods.data,
+        drinks: drinks.data,
+        events: events.data,
+        contact: contact.data,
+        hours: hours.data
+      }
+    })
+  }
 
   render() {
+    let { data } = this.state
+
     return (
       <div className="container">
         <Switch>
-          <Route exact path="/admin" component={Admin} />
+          <Route exact path="/admin" render={() => <Admin hours={data.hours} />} />
           <Route exact path="/admin/login" component={Login} />
           <Route path="" render={() => <Redirect to='/admin/login' />} />
         </Switch>
