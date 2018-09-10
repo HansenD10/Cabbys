6f2c5298f7dc9737560262806be30e2ce5c8c4a1
HansenD10 <root@ip-172-31-23-142.us-east-2.compute.internal>
2018-09-10 03:45:51 +0000

    Fixing bugs in server routing.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,17 @@
 'use strict'
-let apiRoutes = require('../api/routes')
+let apiRoutes = require('../api/routes'),
+    path = require('path')
 
 module.exports = (app) => {
   apiRoutes(app)
 
   //Route to client
   app.get(['/', '/about', '/events', '/menu'], (req,res) => {
-    res.sendFile(path.join(__dirname, '/build/client/index.html'))
+    res.sendFile(path.join(__dirname, '/../build/client/index.html'))
   })
 
   //Route to admin
   app.get(['/admin'], (req,res) => {
-    res.sendFile(path.join(__dirname, '/build/admin/index.html'))
+    res.sendFile(path.join(__dirname, '/../build/admin/index.html'))
   })
 }
