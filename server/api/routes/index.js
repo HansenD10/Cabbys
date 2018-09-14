3716271b65720c6041a42670b709d6572c4822d5
HansenD10 <dylan.hansen10@gmail.com>
2018-09-14 17:17:44 -0500

    Removing post route for contact. use put instead.

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -32,7 +32,6 @@ module.exports = (app) => {
 
   app.route('/api/contact')
     .get(contact.get_contact)
-    .post(contact.new_contact)
     .put(contact.update_contact)
 
   app.route('/api/hours')
