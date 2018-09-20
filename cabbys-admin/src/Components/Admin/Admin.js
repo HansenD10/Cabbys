0440c5ad0c4c8d3326faa4fd201059bd786c7268
HansenD10 <dylan.hansen10@gmail.com>
2018-09-19 21:40:24 -0500

    Adding links section to admin page.

--- cabbys-admin/src/Components/Admin/Admin.js
+++ cabbys-admin/src/Components/Admin/Admin.js
@@ -18,7 +18,7 @@ export default class Admin extends Component {
       return (
         <div>
           <Nav />
-          {contentPage === 'Basic' && this.props.hours && <Basic hours={this.props.hours[0].hours} contact={this.props.contact} />}
+          {contentPage === 'Basic' && this.props.hours && <Basic hours={this.props.hours.hours} contact={this.props.contact} />}
         </div>
       )
     } 
