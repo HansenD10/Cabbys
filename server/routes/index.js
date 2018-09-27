a6f3c73394b9a27bb3d2d7987c30d7006e5c8440
HansenD10 <dylan.hansen10@gmail.com>
2018-09-27 10:56:37 -0500

    Fixing client route.

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,6 +11,7 @@ module.exports = (app) => {
   app.use('/admin', express.static(path.join(__dirname, '/../build/admin')))
   
   // Client Routes 
+  app.use(express.static(path.join(__dirname, '/../build/client')))
   app.use('*', express.static(path.join(__dirname, '/../build/client')))
 
 }
