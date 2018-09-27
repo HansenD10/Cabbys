33ad380c68e77b523facece8c8a9b28e57794440
HansenD10 <dylan.hansen10@gmail.com>
2018-09-27 10:50:07 -0500

    Catching all routes and passing them to client page.

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,14 +6,11 @@ let apiRoutes = require('../api/routes'),
 module.exports = (app) => {
   apiRoutes(app)
 
-  // Client Routes
-  app.use(express.static(path.join(__dirname, '/../build/client')))
-  app.use('/menu', express.static(path.join(__dirname, '/../build/client')))
-  app.use('/events', express.static(path.join(__dirname, '/../build/client')))
-  app.use('/about', express.static(path.join(__dirname, '/../build/client')))
-
   // Admin Routes
-  app.use('/admin', express.static(path.join(__dirname, '/../build/admin')))
   app.use('/admin/login', express.static(path.join(__dirname, '/../build/admin')))
+  app.use('/admin', express.static(path.join(__dirname, '/../build/admin')))
+  
+  // Client Routes 
+  app.use('/*', express.static(path.join(__dirname, '/../build/client')))
 
 }
