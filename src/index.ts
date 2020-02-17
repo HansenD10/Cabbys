0554bbd83bdb012f7d598604c771ea2e892140c5
HansenD10 <dylan.hansen10@gmail.com>
2020-02-16 20:59:57 -0600

    Add listener for google analytics

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ import ReactGA from 'react-ga';
 import App from './App';
 
 ReactGA.initialize("UA-125471451-2");
+ReactGA.pageview(window.location.pathname)
 
 ReactDOM.render(React.createElement(App), document.getElementById('root') as HTMLElement);
