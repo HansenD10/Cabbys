525aaacedfbb77c99c4207be41635c300f232142
HansenD10 <dylan.hansen10@gmail.com>
2020-03-07 22:01:24 -0600

    Add image support for IOS.

--- src/services/image-service.ts
+++ src/services/image-service.ts
@@ -1,3 +1,5 @@
+const isIPhone = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+
 export const transformImage = (imageUrl: string, width: number, height: number): string => {
-  return `${imageUrl}?w=${width}&h=${height}&fm=webp`;
+  return `${imageUrl}?w=${width}&h=${height}${isIPhone ? '&fm=png' : '&fm=webp'}`;
 }
