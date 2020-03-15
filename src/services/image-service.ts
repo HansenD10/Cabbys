438b222a33b95fe9ba4ba7939a6a5d1c27c5890e
HansenD10 <dylan.hansen10@gmail.com>
2020-03-15 13:19:26 -0500

    Add check for iPad to disable webp.

--- src/services/image-service.ts
+++ src/services/image-service.ts
@@ -1,4 +1,12 @@
-const isIPhone = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+const isIPhone = () => {
+  if (/iPad|iPhone|iPod/.test(navigator.platform)) {
+    return true;
+  } else {
+    return navigator.maxTouchPoints &&
+      navigator.maxTouchPoints > 2 &&
+      /MacIntel/.test(navigator.platform);
+  }
+}
 
 export const transformImage = (imageUrl: string, width: number, height: number): string => {
   return `${imageUrl}?w=${width}&h=${height}${isIPhone ? '&fm=png' : '&fm=webp'}`;
